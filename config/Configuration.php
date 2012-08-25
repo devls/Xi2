b34e4bbb9e89326b14f08875966b6d302a35a6d6
Craig @ Home <craig@clearbooks.co.uk>
2012-08-25 17:00:52 +0100

    Configuration stubs

---
+++ config/Configuration.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Xi2.
+ * License: GPLv2/BSD
+ * User: craigjbass
+ * Date: 25/08/12
+ * Time: 16:47
+ */
+namespace Xi2\Configuration;
+
+/**
+ * Do not extend from this class.
+ * Extend from Xi2\Shared\Configuration.
+ */
+abstract class Configuration
+{
+
+    /**
+     * Set global variables here.
+     */
+
+}
