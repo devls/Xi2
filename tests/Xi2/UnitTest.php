ce76ace1c658687d89a53669a90e84293c4c24a2
Craig @ Leviathan <craig@devls.co.uk>
2012-09-04 21:50:06 +0100

    Maybe this will fix the load

---
+++ tests/Xi2/UnitTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Xi2.
+ * License: GPLv2/BSD
+ * User: craigjbass
+ * Date: 04/09/12
+ * Time: 21:48
+ */
+use Xi2\Core\Test\Base as TestCase;
+
+class UnitTest extends TestCase
+{
+
+    /**
+     * A simple test to test that Unit Tests work.
+     */
+    public function testUnitTesting () {
+
+        $this->assertTrue( true );
+
+    }
+
+}
