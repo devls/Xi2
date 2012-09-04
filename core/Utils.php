41300d35746dec6bf52527b93b9114cdccc6b5b6
Craig @ Leviathan <craig@devls.co.uk>
2012-09-04 20:47:20 +0100

    Utils

--- core/Utils.php
+++ core/Utils.php
@@ -6,14 +6,11 @@
  * Date: 27/08/12
  * Time: 11:41
  */
-namespace Xi2\Core;
 
+namespace Xi2; //This is a special class that exists in the Xi2 namespace.
 /**
  * Utility Class Container
  */
-
-namespace Xi2;
-
 class Utils
 {
 
@@ -42,17 +39,36 @@ class Utils
     }
 
     /**
+     * Provides shorthand for isset code structures
+     *
      * @static
      * @param $val
      * @param null $alt
-     * @param Closure $formatter
+     * @param \Closure $formatter
      * @return mixed
      */
-    public static function eisset( &$val, $alt=null, \Closure $formatter=null )
+    public static function eIsSet( &$val, $alt=null, \Closure $formatter=null )
+    {
+        return self::e( function( &$val ) {
+            return isset( $val );
+        }, $val, $alt, $formatter );
+    }
+
+    /**
+     * Provides shorthand for !is_null/ !== null code structures
+     *
+     * @static
+     * @param $val
+     * @param null $alt
+     * @param callable $formatter
+     * @return mixed
+     */
+    public static function eIsNull( &$val, $alt=null, \Closure $formatter=null )
     {
         return self::e( function( &$val ) {
             return $val !== null;
         }, $val, $alt, $formatter );
     }
+
 
 }
