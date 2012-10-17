dea0cc0929d72d1215e65a0cab7e9eb7447dbdc7
Craig @ Leviathan <craig@devls.co.uk>
2012-10-17 19:57:17 +0100

    Reformat the Bootstrap.

--- core/Bootstrap.php
+++ core/Bootstrap.php
@@ -1,190 +1,225 @@
 <?php
 
-namespace Xi2\Core;
-use \Xi2\Core\Libraries\Utils\Generic as Generic;
 /**
- * Xi2.
- * License: GPLv2/BSD
- * User: craigjbass
- * Date: 27/08/12
- * Time: 10:22
+ * Xi2\Core namespace
+ *
+ * @namespace Xi2\Core
  */
-class Bootstrap
-{
-    /* @var \Xi2\Core\Definitions\Kernel */
-    private static $kernel;
+namespace Xi2\Core {
 
-    /* @var array */
-    private static $supports = array();
-
+    use \Xi2\Core\Libraries\Utils\Generic as Generic;
     /**
-     * Includes a file.
-     *
-     * @static
-     * @param $path
-     * @return bool
+     * Xi2.
+     * License: GPLv2/BSD
+     * User: craigjbass
+     * Date: 27/08/12
+     * Time: 10:22
      */
-    public static function doFileInclude( $path )
+    class Bootstrap
     {
+        /* @var \Xi2\Core\Definitions\Kernel */
+        private static $kernel;
 
-        $path = $path.'.php';
-        if( file_exists( $path ) ) {
+        /* @var array */
+        private static $supports = array();
 
-            include $path;
-            return true;
+        /**
+         * Includes a file.
+         *
+         * @static
+         * @param $path
+         * @return bool
+         */
+        public static function doFileInclude( $path )
+        {
 
-        } else {
+            $path = $path.'.php';
+            if( file_exists( $path ) ) {
 
-            return false;
+                include $path;
+                return true;
 
-        }
-    }
+            } else {
 
-    /**
-     * Starts Xi2. Sets up Core class autoloading.
-     *
-     * @param bool $noBoot
-     * @static
-     */
-    public static function go( $noBoot=false )
-    {
-        error_reporting( -1 );
-        gc_enable();
-
-        spl_autoload_register (
-            function( $class ) {
-
-                $class = explode( '\\', $class );
-
-                if( count($class) > 2 && $class[0] == 'Xi2' && $class[1] == 'Core' )
-                {
-                    array_shift( $class ) && array_shift( $class );
-                    Bootstrap::doFileInclude( __DIR__ . '/' . implode( '/', $class ) );
-                }
+                return false;
 
             }
-        );
-        self::$kernel = new Kernel();
-        if( $noBoot ) {
-
-            //Instantiate but do not boot the Kernel. This will cause auto-loaders to be loaded but nothing else.
-            return;
         }
 
-        self::boot(); //Boot will now execute this runcycle.
+        /**
+         * Starts Xi2. Sets up Core class autoloading.
+         *
+         * @param bool $noBoot
+         * @static
+         */
+        public static function go( $noBoot=false )
+        {
+            error_reporting( -1 );
+            gc_enable();
 
-        self::stop(); //When everything is finished, wind it all down.
-    }
+            spl_autoload_register (
+                function( $class ) {
 
-    /**
-     * End a Kernel runtime
-     *
-     * @static
-     */
-    public static function stop()
-    {
-        self::$kernel->halt();
+                    $class = explode( '\\', $class );
 
-        //Force Kernel __destruct
-        self::$kernel = null;
-        gc_collect_cycles();
+                    if( count($class) > 2 && $class[0] == 'Xi2' && $class[1] == 'Core' )
+                    {
+                        array_shift( $class ) && array_shift( $class );
+                        Bootstrap::doFileInclude( __DIR__ . '/' . implode( '/', $class ) );
+                    }
 
-    }
-
-    /**
-     * Protect from fatal errors.
-     *
-     * @static
-     */
-    private static function fatalErrorProtection()
-    {
-        //Generic error handler logic
-        $handler = function( $code, $message, $file, $line ) {
-            echo "Code: {$code}, $message in $file on line $line";
-        };
-
-        //Use it for errors
-        set_error_handler( $handler );
-
-        register_shutdown_function(
-            function() use ( $handler ) {
-                $error = error_get_last();
-                if( $error !== null ) {
-                    $handler( $error['type'], $error['message'], $error['file'], $error['line'] );
                 }
+            );
+            self::$kernel = new Kernel();
+            if( $noBoot ) {
+
+                //Instantiate but do not boot the Kernel. This will cause auto-loaders to be loaded but nothing else.
+                return;
             }
-        );
 
-    }
+            self::boot(); //Boot will now execute this runcycle.
 
-    /**
-     * Boot the Kernel
-     *
-     * @static
-     */
-    private static function boot()
-    {
+            self::stop(); //When everything is finished, wind it all down.
+        }
 
-        try{
+        /**
+         * End a Kernel runtime
+         *
+         * @static
+         */
+        public static function stop()
+        {
+            self::$kernel->halt();
 
-            self::$kernel->boot();
-
-        } catch ( Exception\Exception $e ) {
-
-            $uri = new UriHandler( "Xi2://error/" );
-
-            $kernel = new Kernel( $uri );
-            $kernel->service( 'Exception', $e );
-
-            self::reboot( $kernel );
-
-        } catch ( \Exception $e ) {
-
-            $uri = new UriHandler( "Xi2://error/#unknown" );
-
-            $kernel = new Kernel( $uri );
-            $kernel->service( 'Exception', $e );
-
-            self::reboot( $kernel );
+            //Force Kernel __destruct
+            self::$kernel = null;
+            gc_collect_cycles();
 
         }
 
-    }
+        /**
+         * Returns the currently loaded Kernel instance.
+         *
+         * @return Definitions\Kernel
+         */
+        public static function getKernel()
+        {
 
-    /**
-     * Reboot the Kernel
-     *
-     * @static
-     * @param Definitions\Kernel $kernelInstance
-     */
-    public static function reboot( \Xi2\Core\Definitions\Kernel $kernelInstance )
-    {
-        //If we're replacing the Kernel make sure the previous one knows we are to shutdown without output
-        self::$kernel->shutdown();
-        unset( self::$kernel ); //Unset the old.
+            return self::$kernel;
 
-        //Save and start up the new Kernel.
-        self::$kernel = $kernelInstance;
-        self::boot();
-    }
-
-    /**
-     * Gets or Sets whether $handle is supported.
-     * Where $handle is something that may or may not be supported at runtime
-     * by the system.
-     *
-     * @static
-     * @param $handle
-     * @param bool $value
-     * @return bool
-     */
-    public static function supports( $handle, $value=null )
-    {
-        if( $value !== null ) {
-            self::$supports[ $handle ] = (bool) $value;
         }
 
-        return Generic::eIsSet( self::$supports[ $handle ] );
+        /**
+         * Protect from fatal errors.
+         *
+         * @static
+         */
+        private static function fatalErrorProtection()
+        {
+            //Generic error handler logic
+            $handler = function( $code, $message, $file, $line ) {
+                echo "Code: {$code}, $message in $file on line $line";
+            };
+
+            //Use it for errors
+            set_error_handler( $handler );
+
+            register_shutdown_function(
+                function() use ( $handler ) {
+                    $error = error_get_last();
+                    if( $error !== null ) {
+                        $handler( $error['type'], $error['message'], $error['file'], $error['line'] );
+                    }
+                }
+            );
+
+        }
+
+        /**
+         * Boot the Kernel
+         *
+         * @static
+         */
+        private static function boot()
+        {
+
+            try{
+
+                self::$kernel->boot();
+
+            } catch ( Exception\Exception $e ) {
+
+                $uri = new UriHandler( "Xi2://error/" );
+
+                $kernel = new Kernel( $uri );
+                $kernel->service( 'Exception', $e );
+
+                self::reboot( $kernel );
+
+            } catch ( \Exception $e ) {
+
+                $uri = new UriHandler( "Xi2://error/#unknown" );
+
+                $kernel = new Kernel( $uri );
+                $kernel->service( 'Exception', $e );
+
+                self::reboot( $kernel );
+
+            }
+
+        }
+
+        /**
+         * Reboot the Kernel
+         *
+         * @static
+         * @param Definitions\Kernel $kernelInstance
+         */
+        public static function reboot( \Xi2\Core\Definitions\Kernel $kernelInstance )
+        {
+            //If we're replacing the Kernel make sure the previous one knows we are to shutdown without output
+            self::$kernel->shutdown();
+            unset( self::$kernel ); //Unset the old.
+
+            //Save and start up the new Kernel.
+            self::$kernel = $kernelInstance;
+            self::boot();
+        }
+
+        /**
+         * Gets or Sets whether $handle is supported.
+         * Where $handle is something that may or may not be supported at runtime
+         * by the system.
+         *
+         * @static
+         * @param $handle
+         * @param bool $value
+         * @return bool
+         */
+        public static function supports( $handle, $value=null )
+        {
+            if( $value !== null ) {
+                self::$supports[ $handle ] = (bool) $value;
+            }
+
+            return Generic::eIsSet( self::$supports[ $handle ] );
+
+        }
 
     }
 }
+
+/**
+ * Global Namespace
+ */
+namespace {
+
+    /**
+     * Declare our B (Bootstrap) to be available in the global namespace.
+     */
+    class B extends \Xi2\Core\Bootstrap {
+
+    }
+}
+
+
